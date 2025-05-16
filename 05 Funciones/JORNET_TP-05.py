a6cb56cbde5706c8f8b48800b56e38213dc646f4
juanmjornet <juanjornet.jmj@gmail.com>
2025-05-16 20:57:06 -0300

    Subo TP 05 - reemplazo MI-APELLIDO_TP-05.py por JORNET_TP-05.py

---
+++ 05 Funciones/JORNET_TP-05.py
@@ -0,0 +1,46 @@
+
+
+#Ejercicio 1:
+num_uno_cien = list(range(4, 101, 4))
+print(num_uno_cien)
+
+#Ejercicio 2:
+favoritos = ["futbol", "belgrano de cordoba", "los redondos"]
+print(favoritos[-2])
+
+#Ejercicio 3:
+lista_vacia = []
+lista_vacia.append("Juan")
+lista_vacia.append("Manuel")
+lista_vacia.append("Jornet")
+print(lista_vacia)
+
+#Ejercicio 4:
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "cohete"
+animales[-1] = "pelota"
+print(animales)
+
+#Ejercicio 5:
+"""
+El script de ejemplo crea una lista con 5 elementos numericos en su interior, elimina el maximo
+mediante la funcion max() encuentra el valor maximo de la lista que es 22
+luego con el .remove elimina el valor maximo encontrado y despues de eliminarlo muestra la lista modificada
+"""
+
+#Ejercicio 6:
+num_1_al_30 = list(range(10, 31, 5))
+print(num_1_al_30[:2])
+
+#Ejercicio 7:
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "bicicleta"
+autos[2] = "triciclo"
+print(autos)
+
+#Ejercicio 8:
+dobles = []
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
